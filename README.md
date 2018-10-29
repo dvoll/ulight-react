bb953bd74ad48457445dc9b851eba205a20e1235
DESKTOP-GTE3T9A\Dario <dariovoll@gmail.com>
2018-10-29 15:53:03 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Basic React Component Library ULight
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import BaseButton from "./BaseButton";
+import BaseIcon from "./BaseIcon";
+import BaseInput from "./BaseInput";
+import BaseLabel from "./BaseLabel";
+
+export {BaseButton, BaseLabel, BaseIcon, BaseInput};
