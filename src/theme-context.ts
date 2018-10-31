0c0e89fcebbbcb6cba7992245b117f10c6660d40
DESKTOP-GTE3T9A\Dario <dariovoll@gmail.com>
2018-11-01 00:54:32 +0100

    Theme: Addes Background and Accent Color

--- src/theme-context.ts
+++ src/theme-context.ts
@@ -6,10 +6,14 @@ export const themes = {
         // secondary: '#222222',
         foreground: '220, 220, 220',
         secondary: '74, 74, 74',
+        background: '35, 35, 35',
+        accent: '187, 0, 0',
     },
     light: {
         foreground: '50, 50, 50',
         secondary: '222, 220, 220',
+        background: '248, 248, 248',
+        accent: '187, 0, 0',
     },
 };
 
